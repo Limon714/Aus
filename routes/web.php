7209733058b17c9b84b78c986be4ab731296cc36
Limon Islam <limon4u714@gmail.com>
2024-01-03 23:59:10 +0600

    inventory management

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::post('user-registration',[UserController::class,'UserRegistration']);
+Route::post('user-login',[UserController::class,'UserLogin']);
+Route::post('user-otp',[UserController::class,'SendOtpCode']);
